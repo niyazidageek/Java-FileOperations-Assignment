3899f8204bacda4230bd4d463f1f4a7ca8b80690
niyazidageek <niyazinb@code.edu.az>
2022-04-23 17:08:02 +0400

    feat: exception handling added in part_2 :sparkles:

--- part_2/Main.java
+++ part_2/Main.java
@@ -1,20 +1,23 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    static Path systemPath = Paths.get(System.getProperty("user.dir"));
-
-    static Path dest;
-
     public static void main(String[] args) throws IOException {
 
-        dest = systemPath.resolve(Paths.get(args[args.length-1]));
+        Path systemPath = Paths.get(System.getProperty("user.dir"));
+
+        if(args.length<=1){
+            System.out.println("You should enter at least 2 arguments!");
+            return;
+        }
+
+        Path dest = systemPath.resolve(Paths.get(args[args.length-1]));
 
         if(!Files.exists(dest)){
             new File(dest.toString()).mkdirs();
@@ -22,77 +25,106 @@ public class Main {
 
         for (int i = 0; i < args.length-1; i++) {
             Path _path = systemPath.resolve(Paths.get(args[i]));
-            _copyFilesRecursion(_path);
+            FileManager.copyFilesRecursion(_path, dest);
         }
     }
+}
 
-    private static void _copyFilesRecursion(Path _path, int _depth, Path... _prevPath) throws IOException {
-        if(!Files.exists(_path)) return;
-        printStarted(_path,_depth);
-        if(Files.isDirectory(_path)){
-            File tempDir = Arrays.stream(_prevPath).count()==0
-                    ? new File(dest.resolve(_path.getFileName()).toString())
-                    : new File(_prevPath[0].resolve(_path.getFileName()).toString());
-            tempDir.mkdirs();
-            for (var p:Files.list(_path).collect(Collectors.toList())) {
-                printStarted(p,_depth+3);
-                if(Files.isDirectory(p)){
+
+class FileManager{
+
+    private static Path dest;
+
+    private static void copyFilesRecursion(Path _path, int _depth, Path _prevPath)  {
+        if(dest==null){
+            System.out.println("No destination provided!");
+            return;
+        }
+        try{
+            if(!Files.exists(_path)){
+                PrintManager.printNoSuchDirectoryOrFile(_path, _depth);
+                return;
+            }
+            PrintManager.printStarted(_path,_depth);
+            if(Files.isDirectory(_path)){
+
+                File tempDir = _prevPath == null
+                        ? new File(dest.resolve(_path.getFileName()).toString())
+                        : new File(_prevPath.resolve(_path.getFileName()).toString());
+
+                tempDir.mkdirs();
+                for (var p:Files.list(_path).collect(Collectors.toList())) {
                     try{
-                        File _tempDir = new File(tempDir.toPath().resolve(p.getFileName()).toString());
-                        _tempDir.mkdirs();
-                        for (var a:Files.list(p).collect(Collectors.toList())) {
-                            _copyFilesRecursion(a, _depth+6,_tempDir.toPath());
+                        PrintManager.printStarted(p,_depth+3);
+                        if(Files.isDirectory(p)){
+                            File _tempDir = new File(tempDir.toPath().resolve(p.getFileName()).toString());
+                            _tempDir.mkdirs();
+                            for (var a:Files.list(p).collect(Collectors.toList())) {
+                                copyFilesRecursion(a, _depth+6,_tempDir.toPath());
+                            }
+                            PrintManager.printFinished(p,_depth+3,"FOLDER");
+                        }
+                        else {
+                            Files.copy(p, tempDir.toPath().resolve(p.getFileName()));
+                            PrintManager.printFinished(p,_depth+3,"FILE");
+                            PrintManager.printTotal(p,_depth+3);
                         }
                     }
-                    catch (Exception e){
+                    catch(FileAlreadyExistsException e){
+                        PrintManager.printFileAlreadyExists(p,_depth);
                         continue;
                     }
-                    printFinished(p,_depth+3,"FOLDER");
+                    catch (Exception e){
+                        PrintManager.printException(p,_depth);
+                        continue;
+                    }
                 }
-                else {
-                    Files.copy(p, tempDir.toPath().resolve(p.getFileName()));
-                    printFinished(p,_depth+3,"FILE");
-                    printTotal(p,_depth+3);
-                }
+                PrintManager.printFinished(_path,_depth,"FOLDER");
             }
-            printFinished(_path,_depth,"FOLDER");
+            else{
+                Files.copy(_path, _prevPath==null
+                        ?dest.resolve(_path.getFileName())
+                        :_prevPath.resolve(_path.getFileName()));
+
+                PrintManager.printFinished(_path,_depth,"FILE");
+                PrintManager.printTotal(_path, _depth);
+            }
+        }catch (FileAlreadyExistsException e){
+            PrintManager.printFileAlreadyExists(_path,_depth);
         }
-        else{
-            Files.copy(_path, Arrays.stream(_prevPath).count()==0
-                    ?dest.resolve(_path.getFileName())
-                    :_prevPath[0].resolve(_path.getFileName()));
-            printFinished(_path,_depth,"FILE");
-            printTotal(_path, _depth);
+        catch (Exception e){
+            PrintManager.printException(_path,_depth);
         }
     }
 
-    private static void _copyFilesRecursion(Path _path, Path... _prevPath) throws IOException {
-        _copyFilesRecursion(_path,0,_prevPath);
+    public static void copyFilesRecursion(Path _path, Path _dest){
+        dest = _dest;
+        copyFilesRecursion(_path, 0, null);
     }
+}
 
-    private static String getReadableFileSize(long bytes){
-        if(bytes/1024<1){
-            return String.format("%,dB", bytes);
-        }
-        else if(bytes/1000000<1){
-            return String.format("%,dKB", bytes/1024);
-        }
-        else{
-            return String.format("%,dMB", bytes/1000000);
-        }
-    }
+class PrintManager{
 
-    private static void printStarted(Path path, int depth){
+    private static Path systemPath = Paths.get(System.getProperty("user.dir"));
+
+    private static void generateSpace(int depth){
         for (int i = 0; i < depth; i++) {
             System.out.print(" ");
         }
+    }
+
+    public static void printNoSuchDirectoryOrFile(Path path, int depth){
+        generateSpace(depth);
+        System.out.println(String.format(">There is no such file or directory: %s...",systemPath.relativize(path)));
+    }
+
+    public static void printStarted(Path path, int depth){
+        generateSpace(depth);
         System.out.println(String.format(">Started: %s...",systemPath.relativize(path)));
     }
 
-    private static void printFinished(Path path, int depth, String type){
-        for (int i = 0; i < depth; i++) {
-            System.out.print(" ");
-        }
+    public static void printFinished(Path path, int depth, String type){
+        generateSpace(depth);
         switch (type){
             case "FILE":
                 System.out.println(String.format(">Finished: FILE %s",systemPath.relativize(path)));
@@ -106,10 +138,30 @@ public class Main {
         }
     }
 
-    private static void printTotal(Path path, int depth) throws IOException {
-        for (int i = 0; i < depth; i++) {
-            System.out.print(" ");
-        }
+    public static void printTotal(Path path, int depth) throws IOException {
+        generateSpace(depth);
         System.out.println(String.format(">Total %s were copied!", getReadableFileSize(Files.size(path))));
+    }
+
+    public static void printFileAlreadyExists(Path path, int depth){
+        generateSpace(depth);
+        System.out.println(String.format(">FILE '%s' already exists!", path));
+    }
+
+    public static void printException(Path path, int depth){
+        generateSpace(depth);
+        System.out.println(String.format(">An unexpected error occurred while copying FILE: %s. Continuing...", path));
+    }
+
+    private static String getReadableFileSize(long bytes){
+        if(bytes/1024<1){
+            return String.format("%,dB", bytes);
+        }
+        else if(bytes/1000000<1){
+            return String.format("%,dKB", bytes/1024);
+        }
+        else{
+            return String.format("%,dMB", bytes/1000000);
+        }
     }
 }
